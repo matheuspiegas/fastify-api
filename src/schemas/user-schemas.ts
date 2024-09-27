661ac0b389ade50929a1abcfd5ceecbb508d4493
matheuspiegas <matheusaugustopiegassales@gmail.com>
2024-09-27 13:19:38 -0300

    Refactor user schema validation rules

--- src/schemas/user-schemas.ts
+++ src/schemas/user-schemas.ts
@@ -1,21 +1,37 @@
 import z from "zod";
 
 export const registerRouteSchema = z.object({
-	username: z.string().min(4),
-	password: z.string().min(5),
+	username: z
+		.string()
+		.min(5, { message: "Username must contain at least 5 character(s)" }),
+	password: z
+		.string()
+		.min(5, { message: "Password must contain at least 5 character(s)" }),
 	email: z.string().email(),
-	name: z.string(),
+	name: z
+		.string()
+		.min(3, { message: "Name must contain at least 3 character(s)" }),
 });
 export const loginRouteSchema = z.object({
-	username: z.string(),
-	password: z.string(),
+	username: z
+		.string()
+		.min(5, { message: "Username must contain at least 5 character(s)" }),
+	password: z
+		.string()
+		.min(5, { message: "Password must contain at least 5 character(s)" }),
 });
 
 export const createUserSchema = z.object({
-	username: z.string().min(4),
-	password: z.string().min(5),
+	username: z
+		.string()
+		.min(5, { message: "Username must contain at least 5 character(s)" }),
+	password: z
+		.string()
+		.min(5, { message: "Password must contain at least 5 character(s)" }),
 	email: z.string().email(),
-	name: z.string(),
+	name: z
+		.string()
+		.min(3, { message: "Name must contain at least 3 character(s)" }),
 });
 
 export type CreateUserSchema = z.infer<typeof createUserSchema>;
