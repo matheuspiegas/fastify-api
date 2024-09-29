8ba67fe43bee51f7ddd957576da3e9ba81721069
Matheus Piegas <matheusaugustopiegassales@gmail.com>
2024-09-28 22:24:57 -0300

    Refactor test.js: Update userParam and userDb initialization

---
+++ test.js
@@ -0,0 +1,12 @@
+const userParam = {
+    name: 'John',
+    age: 30
+}
+
+const userDb = {
+    name: 'John',
+    age: 30
+}
+
+const userParamMap  = new Map(Object.entries(userParam));
+const userDbMap  = new Map(Object.entries(userDb));
