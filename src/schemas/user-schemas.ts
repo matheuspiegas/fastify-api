f207296f136adcff6067d6c390b77e7ee64664c1
Matheus Piegas <matheusaugustopiegassales@gmail.com>
2024-09-28 22:24:11 -0300

    Refactor user-schemas.ts: Update registerRouteSchema to registerUserSchema

--- src/schemas/user-schemas.ts
+++ src/schemas/user-schemas.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
-export const registerRouteSchema = z.object({
+export const registerUserSchema = z.object({
 	username: z
 		.string()
 		.min(5, { message: "Username must contain at least 5 character(s)" }),
@@ -21,17 +21,4 @@ export const loginRouteSchema = z.object({
 		.min(5, { message: "Password must contain at least 5 character(s)" }),
 });
 
-export const createUserSchema = z.object({
-	username: z
-		.string()
-		.min(5, { message: "Username must contain at least 5 character(s)" }),
-	password: z
-		.string()
-		.min(5, { message: "Password must contain at least 5 character(s)" }),
-	email: z.string().email(),
-	name: z
-		.string()
-		.min(3, { message: "Name must contain at least 3 character(s)" }),
-});
-
-export type CreateUserSchema = z.infer<typeof createUserSchema>;
+export type RegisterUserSchema = z.infer<typeof registerUserSchema>;
